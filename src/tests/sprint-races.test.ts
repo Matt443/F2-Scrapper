bf75fe802dfab3c075b1cfb7c0b17e95c44b5adf
Matt443 <marcin26050606@gmail.com>
2025-07-08 21:43:43 +0200

    Fixed wrong filename in /src/tests added tests for getSprintResults

---
+++ src/tests/sprint-races.test.ts
@@ -0,0 +1,13 @@
+import { getSprintResults } from '@/scrappers/sprint-results.scrapper';
+
+describe('Testing getSprintResults function', () => {
+    it('Should return sprint results with requsted number id', async () => {
+        expect(await getSprintResults(2024, 1080)).toMatchSnapshot();
+    });
+    it('Should return sprint results with requsted string name of race', async () => {
+        expect(await getSprintResults(2024, 'Sakhir')).toMatchSnapshot();
+    });
+    it('Should throw because this is wrong year', async () => {
+        await expect(getSprintResults(2016)).rejects.toThrow(Error);
+    });
+});
